10da648ccd21de572554c49bfcc1089514adc217
John Berry <ulfmagnetics@gmail.com>
2025-02-02 20:21:50 -0500

    Implement basics of a subscription store using pinia
    The view components have not yet been updated to use the store.

--- src/models/Subscription.ts
+++ src/models/Subscription.ts
@@ -1,6 +1,7 @@
 // Subscription describes a single subscription, i.e. a recurring payment for a service
 export class Subscription {
   constructor(
+    public id: number,
     public serviceName: string,
     public category: string, // TODO: extract this to a separate type
     public cost: number,
@@ -14,7 +15,12 @@ export class Subscription {
   isValid(): boolean {
     // TODO: validate that category is a known value
     // TODO: validate that the next renewal date is in the future if the status is active
-    return this.cost > 0 && this.valueRating >= 1 && this.valueRating <= 5;
+    return (
+      Number.isInteger(this.id) &&
+      this.cost > 0 &&
+      this.valueRating >= 1 &&
+      this.valueRating <= 5
+    );
   }
 
   // TODO: method to return a set of validation errors?
---
+++ src/stores/SubscriptionStore.ts
@@ -0,0 +1,41 @@
+import { defineStore } from 'pinia';
+import { Subscription } from '@/models/Subscription';
+
+export const useSubscriptionStore = defineStore('subscriptions', {
+  state: () => {
+    const stored = localStorage.getItem('subscriptions');
+    const subs = stored ? JSON.parse(stored) : [];
+    return {
+      subscriptions: subs as Subscription[],
+    };
+  },
+
+  actions: {
+    addSubscription(sub: Subscription) {
+      if (!sub.isValid()) throw new Error('Invalid subscription');
+      this.subscriptions.push(sub);
+      this._persistToStorage();
+    },
+
+    updateSubscription(updated: Subscription) {
+      if (!updated.isValid()) throw new Error('Invalid subscription');
+      var existing = this.subscriptions.find((s) => s.id === updated.id);
+      if (!existing) {
+        throw new Error(`no subscription found with id ${updated.id}`);
+      }
+      this.subscriptions.map((s) => (s.id === updated.id ? updated : s));
+      this._persistToStorage();
+    },
+
+    removeSubscription(id: number) {
+      this.subscriptions = this.subscriptions.filter((s) => s.id !== id);
+      this._persistToStorage();
+    },
+
+    _persistToStorage() {
+      localStorage.setItem('subscriptions', JSON.stringify(this.subscriptions));
+    },
+  },
+
+  getters: {},
+});
