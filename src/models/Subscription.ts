eda236049faa080032091daeb922dbc2d30d6a16
John Berry <ulfmagnetics@gmail.com>
2025-02-02 19:36:32 -0500

    Add basics of a Subscription model

---
+++ src/models/Subscription.ts
@@ -0,0 +1,21 @@
+// Subscription describes a single subscription, i.e. a recurring payment for a service
+export class Subscription {
+  constructor(
+    public serviceName: string,
+    public category: string, // TODO: extract this to a separate type
+    public cost: number,
+    public billingCycle: 'monthly' | 'annual',
+    public nextRenewal: Date,
+    public valueRating: number,
+    public notes: string = ''
+    // TODO: add a status field (active, canceled,  expired)
+  ) {}
+
+  isValid(): boolean {
+    // TODO: validate that category is a known value
+    // TODO: validate that the next renewal date is in the future if the status is active
+    return this.cost > 0 && this.valueRating >= 1 && this.valueRating <= 5;
+  }
+
+  // TODO: method to return a set of validation errors?
+}
